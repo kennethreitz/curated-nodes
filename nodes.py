a6d9a6b5dab1d5856d0dfdf546dcb306539555a9
Kenneth Reitz <me@kennethreitz.com>
2013-01-25 21:10:04 -0500

    basic models!

--- nodes.py
+++ nodes.py
@@ -1,7 +1,71 @@
 # -*- coding: utf-8 -*-
 
+import os
+from datetime import datetime
+
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import HSTORE
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+db = SQLAlchemy(app)
+
+class BaseModel(object):
+    def save(self):
+        db.session.add(self)
+        return db.session.commit()
+
+class Expression(db.Model, BaseModel):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), unique=True)
+    content = db.Column(db.Text())
+    slug = db.Column(db.String(140))
+    link = db.Column(db.String(280))
+    draft = db.Column(db.Boolean, default=True)
+    style = db.Column(db.String(80))
+    graduated = db.Column(db.String(280))
+    meta = db.Column(HSTORE())
+    created = db.Column(db.DateTime, default=datetime.now)
+    updated = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Expression %r>' % self.slug
+
+class Experiment(db.Model, BaseModel):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), unique=True)
+    content = db.Column(db.Text())
+    slug = db.Column(db.String(140))
+    link = db.Column(db.String(280))
+    draft = db.Column(db.Boolean, default=True)
+    style = db.Column(db.String(80))
+    status = db.Column(db.String(80))
+    log = db.Column(db.Text())
+    meta = db.Column(HSTORE())
+    created = db.Column(db.DateTime, default=datetime.now)
+    updated = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Experiment %r>' % self.slug
+
+class Exposure(db.Model, BaseModel):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), unique=True)
+    content = db.Column(db.Text())
+    slug = db.Column(db.String(140))
+    link = db.Column(db.String(280))
+    draft = db.Column(db.Boolean, default=True)
+    style = db.Column(db.String(80))
+    meta = db.Column(HSTORE())
+    created = db.Column(db.DateTime, default=datetime.now)
+    updated = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Exposure %r>' % self.slug
+
 
 @app.route('/')
 def hello():
