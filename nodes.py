8db5c8c89a0850bd79137f89c3804bbb839e7265
Kenneth Reitz <me@kennethreitz.com>
2013-01-26 09:12:58 -0500

    expression

--- nodes.py
+++ nodes.py
@@ -124,7 +124,7 @@ def get_exposure(slug):
 
     e = Exposure.query.filter_by(slug=slug, draft=want_drafts).first() or abort(404)
 
-    return e.title
+    return render_template('post.html', post=e, view='exposure')
 
 
 @app.route('/experiments')
@@ -148,7 +148,7 @@ def get_experiment(slug):
 
     e = Experiment.query.filter_by(slug=slug, draft=want_drafts).first() or abort(404)
 
-    return e.title
+    return render_template('post.html', post=e, view='experiment')
 
 
 @app.route('/expressions')
@@ -172,7 +172,7 @@ def get_expression(slug):
 
     e = Expression.query.filter_by(slug=slug, draft=want_drafts).first() or abort(404)
 
-    return e.title
+    return render_template('post.html', post=e, view='expression')
 
 
 if __name__ == '__main__':
